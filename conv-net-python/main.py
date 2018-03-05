df39819837d154d048f6f92517be4e6b8fb7805a
SamKirkiles <sam.kirkiles@gmail.com>
2018-03-04 22:47:32 -0500

    completed backprop on convolution net

--- conv-net-python/main.py
+++ conv-net-python/main.py
@@ -16,8 +16,8 @@ except NameError:
     train_x_raw, train_y, train_y_one_hot = cifar10.load_training_data()
     classes = cifar10.load_class_names()
 
-#m=train_y.shape[0]
-m = 50
+
+m = 2
 
 train_x = train_x_raw[0:m]
 
@@ -28,9 +28,6 @@ plt.show()
 
 train_x  = (train_x -np.mean(train_x ))/(np.max(train_x )-np.min(train_x ))
 
-
-
-#Activation Functions:
     
 def relu(x):
     return np.maximum(0, x)
@@ -38,88 +35,83 @@ def relu(x):
 def relu_back(Z):
     return np.int64(Z > 0)
 
-def conv_forward(a_prev, _filter, parameters):
-    
-    #    activation_caches["conv2"] = conv_forward(activation_caches["pool1"],weight_dict["W2"],parameters)
+def conv_forward(_input,_filter,pad,stride):
 
     
-    stride = parameters['stride']
-    pad = parameters['pad']
-    
-    (m, n_H_prev, n_W_prev, n_C_prev) = a_prev.shape
+    (m, n_h, n_w, n_C_prev) = _input.shape
     (f,f, n_C_prev, n_C) = _filter.shape
    
-    n_H = int(((n_H_prev - f + 2 * pad)/stride)+1)
-    n_W = int(((n_W_prev - f + 2 * pad)/stride)+1)
-    
-    a_prev_pad = np.pad(a_prev, ((0,0),(pad,pad),(pad,pad),(0,0)), 'constant', constant_values=0)
-   
-    Z = np.zeros((m,n_H,n_W,n_C))
+
+    n_H = int(1 + (n_h + 2 * pad - f) / stride)
+    n_W = int(1 + (n_w + 2 * pad - f) / stride)
         
-    
-    for i in range(m):  
+    a_prev_pad = np.pad(_input, ((0,0),(pad,pad),(pad,pad),(0,0)), 'constant', constant_values=0)
+   
+    Z = np.zeros((m, n_H,n_W,n_C))
+            
+    for i in range(m):
         for h in range(n_H):
             for w in range(n_W):
                 for c in range(n_C):
-                    #for channel in range(n_C_prev):
 
-                        
+                            
                         vert_start = h*stride
                         vert_end = vert_start + f
                         horiz_start = w * stride
                         horiz_end = horiz_start + f
                         
-                        #Take a slice of  a and put it in the new volume
+    
                         a_slice = a_prev_pad[i,vert_start:vert_end,horiz_start:horiz_end,:]
                         
-                        Z[i,h,w,c] += np.sum(np.multiply(a_slice, _filter[:,:,:,c]))
-                    
+                        Z[i,h,w,c] = np.sum(np.multiply(a_slice, _filter[:,:,:,c]))
+    
     return Z
 
-def conv_back(_input, dnext, _filter, pad=2, stride=1):
-    
-    #The filter.shape should be the output we are looking for
-    # 9
 
-    # The n_C_prev is the number of channels in the input to this convlution
-    # The n_c is the number of channels in the output layer 
-        
-    #df2 = conv_back(activation_caches["pool1"],dc2,weights["W2"]) 
 
-    (m, n_H, n_W, channels) = _input.shape
+def conv_back(_input,_filter,pad,stride,dout):
+ 
+    (m, n_h, n_w, n_C_prev) = _input.shape
     (f,f, n_C_prev, n_C) = _filter.shape
+   
+    n_H = int(1 + (n_h + 2 * pad - f) / stride)
+    n_W = int(1 + (n_w + 2 * pad - f) / stride)
+    
+    a_prev_pad = np.pad(_input, ((0,0),(pad,pad),(pad,pad),(0,0)), 'constant', constant_values=0)
+   
+    dw = np.zeros(_filter.shape,dtype=np.float32)
+    dx = np.zeros(_input.shape,dtype=np.float32)
 
-    
-    input_pad = np.pad(_input, ((0,0),(pad,pad),(pad,pad),(0,0)), 'constant', constant_values=0)
-
-    
-    empty = np.zeros((f,f, n_C_prev, n_C))
-    
-    for i in range(m):
-        for h in range(f):
-            for w in range(f):
+    for h in range(f):
+        for w in range(f):
+            for p in range(n_C_prev):
                 for c in range(n_C):
-                    #for channel in range(n_C_prev):
 
-                        vert_start = h*stride
-                        vert_end = vert_start + f
-                        horiz_start = w * stride
-                        horiz_end = horiz_start + f
-                         
-                        # df2 = conv_back(activation_caches["pool1"],dc2,weights["W2"]) 
-                        
-                        # We have (50,20,20,4)
-                        # And (50,16,16,4)
-                        
-                        #This is the slice from our original convolution
-                        
-                        a_slice = input_pad[i,vert_start:vert_end,horiz_start:horiz_end,:]
-                        d_slice = dnext[i,h,w,:]
-                                                
-                        empty[h,w,:,c] += np.sum(a_slice * d_slice)
-                  
+
+                    a_slice = a_prev_pad[:,h:h + n_H * stride:stride,w:w + n_W * stride:stride,p]
+
+                    dw[h,w,p,c] = np.sum(a_slice * dout[:,:,:,c])
+    
+    
+    dx_pad = np.pad(dx, ((0,0),(pad,pad),(pad,pad),(0,0)), 'constant', constant_values=0)
+    
+    for i in range(m):        
+        for h_output in range(n_H):
+            for w_output in range(n_W):
+                for g in range(n_C):
+                            
+                    vert_start = h_output*stride
+                    vert_end = vert_start + f
+                    horiz_start = w_output * stride
+                    horiz_end = horiz_start + f
                     
-    return empty
+                    dx_pad[i,vert_start:vert_end,horiz_start:horiz_end,:] += _filter[:,:,:,g] * dout[i,h_output,w_output,g]
+                                    
+                
+    dx = dx_pad[:,pad:pad+n_h,pad:pad+n_w,:]
+                
+    return dw,dx
+
 
 def max_pooling(prev_layer, filter_size=2):
     
@@ -141,17 +133,13 @@ def max_pooling(prev_layer, filter_size=2):
                 
                     prev_slice = prev_layer[i,vert_start:vert_end,horiz_start:horiz_end,c]
                     
-                    pooling[i,w,h,c] = np.max(prev_slice)
+                    pooling[i,h,w,c] = np.max(prev_slice)
                     
     return pooling
 
 
 def max_pooling_back(prev, pool, dmult, filter_size=2):
-    
-    #I think this will be A2 and the size will be (50, 16, 16, 3)
-    
-    #Find the maximum of the pool out and createa mask then multiply this mask with the previous derivative
-    
+        
     (m, n_H, n_W, channels) = pool.shape
     (m_prev, n_prev_H, n_prev_W, channels_prev) = prev.shape
     
@@ -166,8 +154,7 @@ def max_pooling_back(prev, pool, dmult, filter_size=2):
                     vert_end = vert_start + filter_size
                     horiz_start = w*filter_size
                     horiz_end = horiz_start + filter_size
-                    
-                    
+                                        
                     mask = prev[i,vert_start:vert_end,horiz_start:horiz_end,c] == pool[i,h,w,c]
      
                     empty[i,vert_start:vert_end,horiz_start:horiz_end,c] = mask * dmult[i,h,w,c]
@@ -186,19 +173,20 @@ def softmax_back(softmax, Y,m):
 def softmax_cost(y, y_hat):
     return -np.sum(y * np.log(y_hat),axis=1)
 
-def check_gradients(Y,weights,key,dims=False,pool=False):
+
+def check_gradients(weights,key,four_dimensional=False,inline_checker=False):
     
     epsilon = 0.0001
     
     parameters1 = copy.deepcopy(weights)
     parameters2 = copy.deepcopy(weights)
     
-    if pool:
+    if inline_checker:
         weight_dict_1,activation_caches_1,cost1 = forward_propagate(parameters1,1)
         weight_dict_2,activation_caches_2,cost2 = forward_propagate(parameters2,2)
 
     else:
-        if dims == True:
+        if four_dimensional == True:
             parameters1[key][0,0,0,0] += epsilon
             parameters2[key][0,0,0,0] -= epsilon
         else:
@@ -211,34 +199,27 @@ def check_gradients(Y,weights,key,dims=False,pool=False):
     return (cost1 - cost2) / (2. *epsilon)
 
 
-
 def forward_propagate(weight_dict,forward=0):
     
     activation_caches = {}
 
-    parameters={'stride':1,'pad':2 }
-    Z1 = conv_forward(train_x,weight_dict["W1"],parameters)
-    activation_caches["A1"] = relu(Z1)
+    activation_caches["conv1"] = conv_forward(train_x,weight_dict["W1"],2,1)
+    activation_caches["A1"] = relu(activation_caches["conv1"])
     activation_caches["pool1"] = max_pooling(activation_caches["A1"],2)
-    
-    
-    activation_caches["conv2"] = conv_forward(activation_caches["pool1"],weight_dict["W2"],parameters)
-    
-        
-    activation_caches["A2"] = relu(activation_caches["conv2"])
 
-    activation_caches["pool2"]=max_pooling(activation_caches["A2"],2)
-            
+    
+    activation_caches["conv2"] = conv_forward(activation_caches["pool1"],weight_dict["W2"],2,1)   
+    activation_caches["A2"] = relu(activation_caches["conv2"])
+    activation_caches["pool2"]=max_pooling(activation_caches["A2"],2)       
     activation_caches["Ar2"] = activation_caches["pool2"].reshape((m, activation_caches["pool2"].shape[1] * 
                      activation_caches["pool2"].shape[2] * activation_caches["pool2"].shape[3]))
 
     activation_caches["Z3"] = fully_connected(activation_caches["Ar2"], weight_dict["W3"])
     activation_caches["A3"] = relu(activation_caches["Z3"])
     
+    
     activation_caches["Z4"] = fully_connected(activation_caches["A3"],weight_dict["W4"])
-    
     activation_caches["A4"] = softmax(activation_caches["Z4"])
-    
     
     cost = np.mean(softmax_cost(train_y_one_hot[0:m,...], activation_caches["A4"]))
     
@@ -256,7 +237,7 @@ weights["W2"] = np.random.rand(5,5,4,4) * 0.01
 weights["W3"] = np.random.rand(256,10)
 weights["W4"] = np.random.rand(10,10)
 
-weight_dict,activation_caches,cost = forward_propagate(weights)
+weight_dict,activation_caches,cost= forward_propagate(weights)
 
 """
 Back Propagation
@@ -268,59 +249,35 @@ print("\nTesting Backprop \n")
 softmax_grad = softmax_back(activation_caches["A4"], train_y_one_hot[0:m,...], m)
 dw4 = activation_caches["A3"].T.dot(softmax_grad)
 
-grad_W4 = check_gradients(train_y_one_hot[0:m,...], weight_dict,"W4")
-print("First Gradient Approx W4:     " + str(grad_W4))
-print("First Gradient Calculated W4: " + str(dw4[0,0]) + "\n")
+grad_W4 = check_gradients(weight_dict,"W4")
+print("Gradient Approx W4:     " + str(grad_W4))
+print("Gradient Calculated W4: " + str(dw4[0,0]) + "\n")
 
 da3 = (weights["W4"].dot(softmax_grad.T)).T
 dz3 = relu_back(activation_caches["Z3"]) * da3
 dw3 = activation_caches["Ar2"].T.dot(dz3)
 
-grad_W3 = check_gradients(train_y_one_hot[0:m,...], weights,"W3")
-print("Second Gradient Approx W3:     " + str(grad_W3))
-print("Second Gradient Calculated W3: " + str(dw3[0,0]) + "\n")
-
-
+grad_W3 = check_gradients(weights,"W3")
+print("Gradient Approx W3:     " + str(grad_W3))
+print("Gradient Calculated W3: " + str(dw3[0,0]) + "\n")
 
 dpool2 = weights["W3"].dot(dz3.T).T
-#dpool2 = dz3.dot(weights["W3"].T).T
 dpool2_reshape = dpool2.reshape(activation_caches["pool2"].shape)
 
 da2 = max_pooling_back(activation_caches["A2"], activation_caches["pool2"],dpool2_reshape)
-dc2 = relu_back(activation_caches["conv2"]) * da2
-df2 = conv_back(activation_caches["pool1"],dc2,weights["W2"]) 
+dz2 = relu_back(activation_caches["conv2"]) * da2
+df2,dpool1 = conv_back(activation_caches["pool1"],weights["W2"],2,1,dz2) 
 
+grad_W2 = check_gradients(weights,"W2",True,False)
+print("Gradient Approx W2:     " + str(grad_W2))
+print("Gradient Calculated W2: " + str(df2[0,0,0,0]) + "\n")
 
-grad_W2 = check_gradients(train_y_one_hot[0:m,...], weights,"W2", True,False)
-print("Second Gradient Approx W2:     " + str(grad_W2))
-print("Second Gradient Calculated W2: " + str(df2[0,0,0,0]) + "\n")
+da1 = max_pooling_back(activation_caches["A1"], activation_caches["pool1"],dpool1)
+dz1 = relu_back(activation_caches["conv1"]) * da1
+df1,dinput = conv_back(train_x,weights["W1"],2,1,dz1) 
+
+grad_W1 = check_gradients(weights,"W1",True,False)
+print("Gradient Approx W1:     " + str(grad_W1))
+print("Gradient Calculated W1: " + str(df1[0,0,0,0]))
 
 iterations = 100
-
-"""
-for i in range(iterations):
-        
-    weight_dict,activation_caches,cost = forward_propagate(weights)
-    print(cost)
-        
-        
-    softmax_grad = softmax_back(activation_caches["A4"], train_y_one_hot[0:m,...], m)
-    dz4 = activation_caches["A3"].T.dot(softmax_grad)
-    
-    
-    da3 = (weights["W4"].dot(softmax_grad.T)).T
-    dz3 = relu_back(activation_caches["Z3"]) * da3
-    dw3 = activation_caches["Ar2"].T.dot(dz3)
-    
-        
-    dpool2 = weights["W3"].dot(dz3.T)
-    dpool2_reshape = dpool2.reshape(activation_caches["pool2"].shape)
-    
-    da2 = max_pooling_back(activation_caches["A2"], activation_caches["pool2"],dpool2_reshape)
-    dc2 = relu_back(activation_caches["conv2"]) * da2
-    df2 = conv_back(activation_caches["pool1"],dc2,weights["W2"]) 
-    
-    #weights["W4"] -= 0.01 * dz4
-    #weights["W3"] -= 0.01 * dw3
-    weights["W2"] -= 0.01 * df2
-"""
